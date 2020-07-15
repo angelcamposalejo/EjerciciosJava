78ae30a904fd3ab644b7d445c81cfef35201419a
Jose Angel Campos Alejo <javierserranolule@MacBook-Pro-de-Jose.local>
2020-07-15 10:09:00 -0500

    ejercicio modificadores de acceso protected

---
+++ ModificadorProtected/src/main/java/paqueteUno/Atributo_Metodos.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paqueteUno;
+
+/**
+ *
+ * @author angelcampos
+ */
+public class Atributo_Metodos {
+    protected int valorUno;
+    protected int valorDos;
+    protected String valorTres;
+    
+    protected void metodoAngel(){
+        
+    }
+}
