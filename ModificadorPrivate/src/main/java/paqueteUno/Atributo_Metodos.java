34fb59c723979e0f579b36343a4c74ce7bf5184e
Jose Angel Campos Alejo <javierserranolule@MacBook-Pro-de-Jose.local>
2020-07-15 09:55:15 -0500

    ejercicio modificador de acceso private

---
+++ ModificadorPrivate/src/main/java/paqueteUno/Atributo_Metodos.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paqueteUno;
+
+/**
+ *
+ * @author angelcampos
+ */
+public class Atributo_Metodos {
+    private int valorUno;
+    private int valorDos;
+    private String valortres;
+    
+    private void metodoAngel(){
+        
+    }
+
+    /**
+     * @return Devuelve el atributo valorUno
+     */
+    public int getValorUno() {
+        return valorUno;
+    }
+
+    /**
+     * @param valorUno Asigna el atributo valorUno
+     */
+    public void setValorUno(int valorUno) {
+        this.valorUno = valorUno;
+    }
+    
+    
+}
